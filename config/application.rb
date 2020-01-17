aafc3c086eee8381e66556235d8efef994ebcdd2
Jorge Durango <djorge.ivan97@gmail.com>
2020-01-17 12:36:40 -0500

    changed ui origin

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module CheckingApp
 
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000'
+        origins 'https://checkingappui.herokuapp.com/'
         resource '*',
           headers: :any,
           methods: %i(get post put patch delete options head)
