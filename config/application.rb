5c9c8d0e2721045ad32e9b3e0fe8d2fa3d839ee7
Jorge Durango <djorge.ivan97@gmail.com>
2020-01-17 15:08:33 -0500

    accepting connections from localhost too

--- config/application.rb
+++ config/application.rb
@@ -37,10 +37,17 @@ module CheckingApp
 
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://checkingappui.herokuapp.com'
+        origins 'https://checkingappui.herokuapp.com '
         resource '*',
           headers: :any,
           methods: %i(get post put patch delete options head)
+      end
+
+      allow do
+        origins 'http://localhost:3000'
+        resource '*',
+        headers: :any,
+        methods: %i(get post put patch delete options head)
       end
     end
   end
